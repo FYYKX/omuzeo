55c440b65885b082fc10444f60cc6e7a70a0591f
stella sofia <stellasis@spgroup.com.sg>
2021-10-17 19:40:09 +0800

    add dockerised dev wallet and emulator

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React, {useEffect} from 'react';
-import "./config"
-import Authenticate from "./Authenticate";
+import React, { useEffect } from 'react';
+import './config';
+import Authenticate from './Authenticate';
 
 const App = () => {
-    return <>
-        <div className="App">Omuseo home</div>
-        <Authenticate />
+  return (
+    <>
+      <div className="App">Omuseo home....</div>
+      <Authenticate />
     </>
-}
+  );
+};
 
 export default App;
--- src/config.js
+++ src/config.js
@@ -1,12 +1,12 @@
-import { config } from '@onflow/fcl';
+import * as fcl from '@onflow/fcl';
 
-// TODO: add correct env for production deployment
-config()
-  .put('challenge.scope', 'email') // request for Email
-  .put('accessNode.api', 'https://access-testnet.onflow.org') // Flow testnet
-  .put('discovery.wallet', 'https://flow-wallet-testnet.blocto.app/api/flow/authn') // Blocto testnet wallet
-  .put('discovery.wallet.method', 'HTTP/POST')
-  .put('service.OpenID.scopes', 'email!');
-// config for fcl dev wallet
-// .put("accessNode.api", "http://localhost:8080")
-// .put("challenge.handshake", "http://localhost:8701/flow/authenticate")
+// TODO: add correct env for qa deployment with blocto wallet
+// .put('challenge.scope', 'email') // request for Email
+// .put('accessNode.api', 'https://access-testnet.onflow.org') // Flow testnet
+// .put('discovery.wallet', 'https://flow-wallet-testnet.blocto.app/api/flow/authn') // Blocto testnet wallet
+// .put('discovery.wallet.method', 'HTTP/POST')
+// .put('service.OpenID.scopes', 'email!');
+fcl
+  .config()
+  .put('accessNode.api', 'http://localhost:8080')
+  .put('challenge.handshake', 'http://localhost:8701/fcl/authn');
