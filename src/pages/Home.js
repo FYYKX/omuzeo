3c312df88b5e7c14330d5ae72a688d93f3ecaefe
stella sofia <stellasis@spgroup.com.sg>
2021-10-21 22:41:41 +0800

    add homepage placeholder

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,60 +1,48 @@
-import React from 'react';
-import SearchIcon from '@mui/icons-material/Search';
-import { Box, Stack, TextField } from '@mui/material';
-import Button from '@mui/material/Button';
-
-function Item(props) {
-  const { sx, ...other } = props;
-  return (
-    <Box
-      sx={{
-        bgcolor: 'primary.main',
-        color: 'white',
-        p: 1,
-        borderRadius: 1,
-        textAlign: 'center',
-        fontSize: 19,
-        fontWeight: '700',
-        ...sx,
-      }}
-      {...other}
-    />
-  );
-}
+import React, { useState } from 'react';
+import { InputAdornment, TextField } from '@mui/material';
+import IconButton from '@mui/material/IconButton';
+import { Search } from '@mui/icons-material';
+import QuiltedImageList from "../components/QuiltedImageList";
 
 const Home = () => {
+  const [searchText, setSearchText] = useState('');
+
+  const handleSearchTextChange = (evt) => {
+    setSearchText(evt.target.value);
+  };
+
+  const handleSearchTextEnter = (evt) => {
+    if (evt.keyCode === 13) {
+      // TODO: send api request to backend!
+    }
+  };
+
+  const handleSearchTextClick = (evt) => {
+    // TODO: send api request to backend!
+  };
+
   return (
     <>
-      <div style={{ width: '100%' }}>
-        <Box
-          sx={{
-            display: 'grid',
-            gridAutoColumns: '1fr',
-            gap: 1,
-          }}
-        >
-          <Item sx={{ gridRow: '1', gridColumn: 'span 2' }}>span 2</Item>
-          {/* The second non-visible column has width of 1/4 */}
-          <Item sx={{ gridRow: '1', gridColumn: '4 / 5' }}>4 / 5</Item>
-        </Box>
-        <Stack direction="row">
-          <TextField placeholder="Search" style={{minWidth: 500}}/>
-          <Button onClick={() => {}}>
-            <SearchIcon />
-          </Button>
-        </Stack>
-        <Box sx={{ display: 'flex', p: 1, bgcolor: 'background.paper' }}>
-          <Box sx={{ p: 1, flexGrow: 1, bgcolor: 'grey.300' }}>
-            {' '}
-            <TextField placeholder="Search" />
-          </Box>
-          <Box sx={{ p: 1, bgcolor: 'grey.300' }}>
-            <Button onClick={() => {}}>
-              <SearchIcon />
-            </Button>
-          </Box>
-        </Box>
-      </div>
+      <br />
+      <TextField
+        fullWidth
+        label="Search"
+        value={searchText}
+        onChange={handleSearchTextChange}
+        onKeyUp={handleSearchTextEnter}
+        onClick={handleSearchTextClick}
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">
+              <IconButton>
+                <Search />
+              </IconButton>
+            </InputAdornment>
+          ),
+        }}
+      />
+      <br />
+      <QuiltedImageList />
     </>
   );
 };
